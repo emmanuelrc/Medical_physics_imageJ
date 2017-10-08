38cff4a4c188284f8a92a52aa74f6a8634b02938
emmanuelrc <ema1231929@gmail.com>
2017-10-08 00:32:53 -0500

    added new comment that does

--- dicom_header.py
+++ dicom_header.py
@@ -1,6 +1,7 @@
 from ij import IJ
 from ij.util import DicomTools
 
-imp = IJ.getImage()   
+imp = IJ.getImage()
 studyDescription = DicomTools.getTag(imp, "0008,1030")
 print("Study Description: "+ studyDescription)
+#new comment that doesn't do enything
